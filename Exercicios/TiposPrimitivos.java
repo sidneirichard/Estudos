fb3447015208d2ed9a2ac2dce7ca2be7fe96bb1b
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-05-28 04:49:58 -0300

    sem out

--- Exercicios/TiposPrimitivos.java
+++ Exercicios/TiposPrimitivos.java
@@ -1,5 +1,11 @@
 public class TiposPrimitivos {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        if (true) System.out.println("nice!");
+
+        String resposta;
+
+    resposta =  (1<2) ? "sim" : "nao";
+        System.out.println(resposta);
     }
 }
