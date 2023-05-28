d66a79c9951dc3d9afb17a9b77598292dec1d138
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-05-28 03:50:46 -0300

    teste tipos

---
+++ Java/TesteTipos/src/TesteTipos.java
@@ -0,0 +1,10 @@
+public class TesteTipos {
+    public static void main(String[] args) {
+        String valor = "30";
+        int idade = Integer.parseInt(valor);
+        int resultado = 10 + idade;
+        System.out.println((valor)+(idade+1));
+        if (true) {
+        }
+    }
+}
