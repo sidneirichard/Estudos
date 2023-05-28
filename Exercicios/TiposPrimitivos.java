c6ed9f74531eab4cc2ee266f1114d978965cc62a
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-05-28 12:10:38 -0300

    mudança 01

--- Exercicios/TiposPrimitivos.java
+++ Exercicios/TiposPrimitivos.java
@@ -1,12 +1,14 @@
+import java.util.Scanner;
+
 public class TiposPrimitivos {
+    static Scanner teclado = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        if (true) System.out.println("nice!");
-
-        String resposta;
-
-    resposta =  (1<2) ? "sim" : "nao";
-        System.out.println(resposta);
-        
+        System.out.printf("Digite o nome do aluno: ");
+        String nome = teclado.nextLine();
+        System.out.printf("Digite a nota do aluno: ");
+        float nota = teclado.nextFloat();
+        System.out.printf("A nota de %s é %.1f \n",nome, nota);
     }
 }
+
