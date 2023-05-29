d8249193ff9d8fc497ef08e220ec7bc7d167662b
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-05-29 20:03:02 -0300

    aulas repetição

--- Exercicios/EstruturasRepeticao.java
+++ Exercicios/EstruturasRepeticao.java
@@ -3,8 +3,11 @@ public class EstruturasRepeticao {
         int cc = 0;
         while (cc<10){
             cc++;
-            if (cc==5||cc==7||cc==9){
-                continue;
+            if (cc==2||cc==3||cc==4){
+                continue; // modificador de fluxo de laço/joga pra dentro do laço
+            }
+            if(cc==7){
+                break; // modificador de fluxo de laço/ joga pra fora do laço
             }
             System.out.println(cc);
         }
---
+++ Exercicios/Repeticao03.java
@@ -0,0 +1,7 @@
+public class Repeticao03 {
+    public static void main(String[] args) {
+        for (int cc=0;cc<=3;cc++){
+            System.out.println("Cambalhota "+cc);
+        }
+    }
+}
