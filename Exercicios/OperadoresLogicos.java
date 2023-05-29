4186973b26269bd105f939b39b8902c424aec777
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-05-29 15:39:07 -0300

    aulas operadores

--- Exercicios/OperadoresLogicos.java
+++ Exercicios/OperadoresLogicos.java
@@ -1,11 +1,13 @@
 public class OperadoresLogicos {
     public static void main(String[] args) {
         int x, y, z;
-        x = 14;
-        y = 17;
-        z = 112;
+        x = 4;
+        y = 7;
+        z = 12;
         boolean r;
-        r = (x<y && y<z)?true:false; // nesse caso x é menor que y 'E' y é menor que z  (&& operador logico 'E')//s
+        r = (x<y ^ y<z)?true:false;  // nesse caso é mostrado 'false' pq x é menor que y e y tbm é menor que z, nao sendo exclusivos  (^ operador logico 'EXCLUSIVO')//
+        /*r = (x<y || y==z)?true:false;*/  // nesse caso printa true pq x é menor que y 'OU' y é igual a z  (|| operador logico 'OU')//
+        /*r = (x<y && y<z)?true:false;*/  // nesse caso x é menor que y 'E' y é menor que z  (&& operador logico 'E')//
         System.out.println(r);
     }
 }
