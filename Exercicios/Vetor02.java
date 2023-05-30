3bf8c0fa3c8984f04bd58efd102141e01da17639
Sidnei Richard <sidneirichardpagliaroni@gmail.com>
2023-05-30 20:11:28 -0300

    vetor02

--- Exercicios/Vetor02.java
+++ Exercicios/Vetor02.java
@@ -10,8 +10,6 @@ public class Vetor02 {
         for(int c=0; c<mes.length;c++){
             System.out.println("O mês de " +mes[c]+" tem "
                     +tot[c] +" dias ao todo.");
-
-            
         }
     }
 }
