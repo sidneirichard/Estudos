556683cff7691ce8510f8c5690dd331731dc0745
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-05-30 01:09:41 -0300

    laços aninhados again

--- Exercicios/LacosAninhados.java
+++ Exercicios/LacosAninhados.java
@@ -2,7 +2,7 @@ public class LacosAninhados {
     public static void main(String[] args) {
         for (int i=1;i<=3;i++){
             for (int j=0;j<=2;j+=2){
-                System.out.println(i+ "," +j+""); // para printar as variaveis i,j
+                System.out.println(i+ "," +j+""); // para printar as variaveis i,j i+,+j+""
             }
         }
     }
