12581c7e305ec44fbefa7a9627d572e08413b539
Sidnei Richard <sidneirichardpagliaroni@gmail.com>
2023-05-30 20:41:37 -0300

    nova mudança no vetor02

--- Exercicios/Vetor02.java
+++ Exercicios/Vetor02.java
@@ -1,6 +1,9 @@
 import java.time.Year;
 
 public class Vetor02 {
+    public static boolean bissexto (int ano){
+        return (ano % 4 == 0) && (ano % 100 !=0 || ano % 400 == 0);
+    }
     public static void main(String[] args) {
         String mes[] = {"Jan","Fev","Mar","Abr","Mai",
                         "Jun","Jul","Ago","Set","Out","Nov","Dez"};  //  'ctrl+enter' para quebra de linha
