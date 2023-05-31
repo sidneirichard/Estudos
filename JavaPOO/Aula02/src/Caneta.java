38e113beb55c2d70d65dd9b61710b158cc833939
Sidnei Richard <sidneirichardpagliaroni@gmail.com>
2023-05-31 19:05:46 -0300

    commit da aula02 POO

--- JavaPOO/Aula02/src/Caneta.java
+++ JavaPOO/Aula02/src/Caneta.java
@@ -1,18 +1,18 @@
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
 
-    void status(){
+    public void status(){
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta: " + this.cor);
         System.out.println("Ponta: " + this.ponta);
         System.out.println("Carga: " + this.carga);
         System.out.println("Está tampada?: " + this.tampada);
     }
-    void rabiscar(){
+    public void rabiscar(){
         if(this.tampada == true){
             System.out.println("ERRO! Não posso rabiscar.");
         }
@@ -21,9 +21,9 @@ public class Caneta {
         }
     }
 
-    void tampar(){ this.tampada = true;}
+    public void tampar(){ this.tampada = true;}
 
-    void destampar(){this.tampada = false;}
+    public void destampar(){this.tampada = false;}
 
 
 }
