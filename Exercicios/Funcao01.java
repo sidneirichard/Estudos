9de32925fc629998ebdab874c6f26bae64664943
Sidnei Richard <sidneirichardpagliaroni@gmail.com>
2023-05-31 12:34:55 -0300

    termino java iniciante

--- Exercicios/Funcao01.java
+++ Exercicios/Funcao01.java
@@ -1,11 +1,12 @@
 public class Funcao01 {
-    static void soma (int a, int b){
+    static int soma (int a, int b){
         int s = a+b;
-        System.out.println("A soma é "+s);
+        return s;
     }
     public static void main(String[] args) {
         System.out.println("O método 'main' é executado antes do procedimento 'soma'");
-        soma(5,2);
+        int sm = soma(999,1);
+        System.out.println("A soma é: "+sm);
 
     }
 }
