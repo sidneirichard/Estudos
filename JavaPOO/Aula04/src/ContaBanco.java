8e498ca456b475617a9dab639940a0127755b02d
Sidnei Richard <sidneirichardpagliaroni@gmail.com>
2023-06-01 18:15:08 -0300

    commit aula04 java POO

---
+++ JavaPOO/Aula04/src/ContaBanco.java
@@ -0,0 +1,131 @@
+// ATRIBUTOS
+    public class ContaBanco {
+    public int numConta;
+    protected String tipoConta;
+    private String Dono;
+    private float saldoConta;
+    private boolean status;
+
+    {
+
+    }
+    // Métodos Personalizados
+
+    public void estadoAtual() {
+        System.out.println("----------------------------");
+        System.out.println("Conta: " + this.getNumConta());
+        System.out.println("Tipo: " + this.getTipoConta());
+        System.out.println("Usuário: " + this.getDono());
+        System.out.println("Saldo da conta: " + this.getSaldoConta());
+        System.out.println("Status da conta: " + this.isStatus());
+    }
+
+    public void abrirConta(String t) {
+        this.setTipoConta(t);
+        this.setStatus(true);
+        if ("cc".equals(t)) {
+            this.setSaldoConta(50f);
+        } else if ("cp".equals(t)) {
+            this.setSaldoConta(150f);
+        }
+        System.out.println("Conta aberta com sucesso!");
+    }
+
+    public void fecharConta() {
+        if (this.getSaldoConta() >= 0) {
+            System.out.println("Conta com saldo positivo.");
+        } else if (this.getSaldoConta() < 0) {
+            System.out.println("Conta com saldo negativo.");
+        } else {
+            this.setStatus(false);
+            System.out.println("Conta fechada com sucesso.");
+        }
+    }
+
+    public void depositar(float v) {
+        if (this.isStatus()) {
+            this.setSaldoConta(this.getSaldoConta() + v);
+            System.out.println("Deposito realizado na conta de " + this.getDono());
+        } else {
+            System.out.println("Conta não existente. Impossível depositar.");
+        }
+    }
+
+    public void sacar(float v) {
+        if (this.isStatus()) {
+            if (this.getSaldoConta() >= v) {
+                this.setSaldoConta(this.getSaldoConta() - v);
+                System.out.println("Saque realizado na conta de " + this.getDono());
+            } else {
+                System.out.println("Saldo insuficiente para saque");
+            }
+        } else {
+            System.out.println("Conta inexistente. Impossível sacar");
+        }
+    }
+
+    public void anuidade() {
+        int v = 0;
+        if (this.getTipoConta() == "cc") {
+            v = 12;
+        } else {
+            if (this.getTipoConta() == "cp") {
+                v = 20;
+            }
+            if (this.isStatus()) {
+                this.setSaldoConta(this.getSaldoConta() - v);
+                System.out.println("Anuidade paga com sucesso por " + this.getDono());
+            } else {
+                System.out.println("Conta Inexistente. Impossível pagar anuidade.");
+            }
+        }
+    }
+
+    // Metodos especiais
+    public void ContaBanco(float saldoConta, boolean status) {
+        this.setSaldoConta(0);
+        this.setStatus(false);
+    }
+
+    public int getNumConta() {
+        return this.numConta;
+    }
+
+    public void setNumConta(int n) {
+        this.numConta = n;
+    }
+
+    public String getTipoConta() {
+        return tipoConta;
+    }
+
+
+
+        public void setTipoConta(String tc) {
+            this.tipoConta = tipoConta;
+        }
+
+        public String getDono() {
+            return Dono;
+        }
+
+        public void setDono(String dc){
+            this.getDono();
+        }
+
+        public float getSaldoConta() {
+            return saldoConta;
+        }
+
+        public void setSaldoConta(float saldoConta) {
+            this.saldoConta = saldoConta;
+        }
+
+        public boolean isStatus() {
+            return status;
+        }
+
+        public void setStatus(boolean status) {
+            this.status = status;
+        }
+    }
--- JavaPOO/Aula04/src/aula04ContaBanco.java
+++ JavaPOO/Aula04/src/aula04ContaBanco.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class aula04ContaBanco {
+public class aula04ContaBanco {
+    public static void main(String[] args) {
+        ContaBanco p1 = new ContaBanco();
+        p1.setNumConta(1111);
+        p1.setDono("Sidnei");
+        p1.abrirConta("cc");
+        p1.estadoAtual();
+
+
+
+    }
 }
