c6d13d7c0319775a84c0b4d29fe300efe945f1d4
Sidnei Richard <sidneirichardpagliaroni@gmail.com>
2023-06-07 00:59:46 -0300

    ex006 python

--- Python/AulasPython/ex004.py
+++ Python/AulasPython/ex004.py
@@ -1,7 +1,7 @@
 n = input('Digite algo: ')
 print('O tipo primitivo desse valor é', type(n))
 print('Contém apenas espaços?', n.isspace())
-print('è um número?', n.isnumeric())
+print('É um número?', n.isnumeric())
 print('É alfabético?', n.isalpha())
 print('É minúsculo?', n.islower())
 print('É maiúscula?', n.isupper())
---
+++ Python/AulasPython/ex006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+d = n * 2
+t = n * 3
+r = n ** (1 / 2)
+print('O dobro de {} é {}, '.format(n, (n*2)))
+print('O triplo de {} é {} e \nA raiz quadrada de {} é {:.2f}'.format(n, (n*3), n, (n**(1/2)))) #pow(n, (1/2)) tbm acha a raiz quadrada
