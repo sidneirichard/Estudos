7425896f590ce8af2c746fa7641ca43fde55be33
Sidnei Richard Pagliaroni <sidneirichardpagliaroni@gmail.com>
2023-06-10 23:41:20 -0300

    commit loop repetiçao

--- NovoJava/MegaSena/src/MegaSena.java
+++ NovoJava/MegaSena/src/MegaSena.java
@@ -4,7 +4,7 @@ public class MegaSena {
     public static void main(String[] args) {
         Random generated = new Random();
         for (int i = 0; i < 6; i++) {
-            int num = generated.nextInt(60);
+            int num = generated.nextInt(20);
             System.out.println(num);
         }
     }
