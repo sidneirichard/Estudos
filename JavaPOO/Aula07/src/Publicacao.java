5058c9767505f1e349613a815f98b5a0503aa531
Sidnei Richard Pagliaroni <sidneirichardpagliaroni@gmail.com>
2023-06-11 13:17:38 -0300

    deletando .idea do repo

--- JavaPOO/Aula07/src/Publicacao.java
+++
@@ -1,8 +0,0 @@
-public interface Publicacao {
-    public void abrir();
-    public void fechar();
-    public void folhear(int p);
-    public void avancarPag();
-    public  void voltarPag();
-
-}
