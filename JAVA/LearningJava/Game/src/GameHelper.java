5585e2cfac7c3f51f1e7f5af8a8f1f1bc3f48d83
Sidnei Richard Pagliaroni <sidneirichardpagliaroni@gmail.com>
2023-06-16 00:48:51 -0300

    commit SimpleStartup com comentarios

--- JAVA/LearningJava/Game/src/GameHelper.java
+++ JAVA/LearningJava/Game/src/GameHelper.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class GameHelper {
     public int getUserInput(String prompt) {
-        System.out.println(prompt + ": ");
-        Scanner scanner = new Scanner(System.in);
-        return scanner.nextInt();
+        System.out.println(prompt + ": ");  // Chama o 'prompt' para interação do usuário
+        Scanner scanner = new Scanner(System.in);  // // Cria uma instância do Scanner para ler a entrada do usuário
+        return scanner.nextInt();  // Lê e retorna o próximo número inteiro fornecido pelo usuário
     }
 }
--- JAVA/LearningJava/Game/src/SimpleStartupTestDrive.java
+++ JAVA/LearningJava/Game/src/SimpleStartupTestDrive.java
@@ -5,19 +5,20 @@ public class SimpleStartupTestDrive {
     public static void main(String[] args) {
         int numOfGuess = 0;
         Random random = new Random();
-        SimpleStartup dot = new SimpleStartup();
-        int startingLocation = random.nextInt(5);
-        int[] locations = new int[]{startingLocation, startingLocation + 1, startingLocation + 2};
-        dot.setLocationsCells(locations);
-        boolean isAlive = true;
+        SimpleStartup dot = new SimpleStartup();  // Instância da classe SimpleStartup
+        int startingLocation = random.nextInt(5);  // Gera um número aleatório entre 0 e 4
+        int[] locations = new int[]{startingLocation,
+                startingLocation + 1, startingLocation + 2};  // Cria um Array com as posições das células
+        dot.setLocationsCells(locations);  // Define as cúlulas no objeto SimpleStartup
+        boolean isAlive = true;  // Variável para controlar o estado do jogo
         while (isAlive) {
             Scanner scanner = new Scanner(System.in);
             System.out.print("Enter your guess: ");
-            int userGuess = scanner.nextInt();
-            String result = dot.checkYourself(userGuess);
-            numOfGuess++;
-            if (result.equals("kill")) {
-                isAlive = false;
+            int userGuess = scanner.nextInt();  // Obtém o palpite do usuário
+            String result = dot.checkYourself(userGuess);  // Verifica o palpite do usuário
+            numOfGuess++;  // Incrementa o número de palpites
+            if (result.equals("kill")) {  // Verifica se o resultado é 'kill'
+                isAlive = false;  // Finaliza o loop
                 System.out.println("You took " + numOfGuess + " guesses.");
             }
         }
