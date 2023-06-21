97f68b58ebe8bca37d2c455b80ecd47125dd85c1
Sidnei Richard Pagliaroni <134716230+sidneirichard@users.noreply.github.com>
2023-06-21 13:33:04 -0300

    Delete JAVA/LearningJava/XCopy directory

--- JAVA/LearningJava/XCopy/src/XCopy.java
+++
@@ -1,8 +0,0 @@
-public class XCopy {
-    public int orig;
-
-    public int go(int arg){
-        arg = arg * 2; // atribuindo o resultado de 'arg * 2' ao parametro 'int arg' criada no metodo 'int go(int arg)'
-        return arg;
-    }
-}
--- JAVA/LearningJava/XCopy/src/XCopyTest.java
+++
@@ -1,8 +0,0 @@
-public class XCopyTest {
-    public static void main(String[] args) {
-        int orig = 42;
-        XCopy x = new XCopy();
-        int y = x.go(orig);
-        System.out.println(orig + " " + y);
-    }
-}
