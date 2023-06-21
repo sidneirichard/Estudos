cc0bb9b8e7323488f682ef2ca6d975a0c767614c
Sidnei Richard Pagliaroni <134716230+sidneirichard@users.noreply.github.com>
2023-06-21 13:29:54 -0300

    Delete JAVA/LearningJava/Episode directory

--- JAVA/LearningJava/Episode/src/EpisodeTestDrive.java
+++
@@ -1,9 +0,0 @@
-public class EpisodeTestDrive {
-    public static void main(String[] args) throws Exception {
-        Episode episode = new Episode();
-        episode.seriesNumber = 4;
-        episode.episodeNumber = 17;
-        episode.play();
-        episode.skipIntro();
-    }
-}
