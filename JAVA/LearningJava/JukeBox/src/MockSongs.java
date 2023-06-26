8b3b184eb10879302d8cf9174831caedc33ccb38
Sidnei Richard Pagliaroni <sidneirichardpagliaroni@gmail.com>
2023-06-26 01:51:07 -0300

    commits da madrugada

--- JAVA/LearningJava/JukeBox/src/MockSongs.java
+++ JAVA/LearningJava/JukeBox/src/MockSongs.java
@@ -7,10 +7,11 @@ public class MockSongs {
         songs.add(new SongsV2("somersault", "zero 7", 147));
         songs.add(new SongsV2("cassidy", "grateful dead", 158));
         songs.add(new SongsV2("$10", "hitchhiker", 140));
-        songs.add(new SongsV2("havana", "cabello", 105));
+        songs.add(new SongsV2("havana", "Cabello", 105));
         songs.add(new SongsV2("Cassidy", "grateful dead", 158));
         songs.add(new SongsV2("50 Ways", "simon", 102));
         songs.add(new SongsV2("Bright Eyes", "Blind Guardian", 94));
+        songs.add(new SongsV2("Blood Brothers", "Iron Maiden", 104));
         return songs;
     }
 }
