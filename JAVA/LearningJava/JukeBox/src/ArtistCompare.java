4aac27fb9e96f4370d4361366f08b099cea6132f
Sidnei Richard Pagliaroni <134716230+sidneirichard@users.noreply.github.com>
2023-06-28 16:30:51 -0300

    Delete JAVA/LearningJava/JukeBox directory

--- JAVA/LearningJava/JukeBox/src/ArtistCompare.java
+++
@@ -1,7 +0,0 @@
-import java.util.Comparator;
-
-public class ArtistCompare implements Comparator<SongsV3> {
-    public int compare(SongsV3 one, SongsV3 two){
-        return one.getArtist().compareTo(two.getArtist());
-    }
-}
