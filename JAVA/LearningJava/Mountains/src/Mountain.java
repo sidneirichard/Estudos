6ad337f1464aa0970de9640702749578a1d7e124
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-06-29 12:13:30 -0300

    mountains lambda sort

---
+++ JAVA/LearningJava/Mountains/src/Mountain.java
@@ -0,0 +1,16 @@
+public class Mountain {
+    String name;
+    int height;
+
+
+
+    public Mountain(String name, int height) {
+        this.name = name;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + height;
+    }
+}
