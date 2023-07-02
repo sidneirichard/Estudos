16eded6a75ab05172cec8e99caaa6f88a9ef9a60
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-07-01 23:56:58 -0300

    Aplicativo lista de tarefas simples

--- JAVA/LearningJava/Mountains/src/SortMountains.java
+++ JAVA/LearningJava/Mountains/src/SortMountains.java
@@ -12,6 +12,7 @@ public class SortMountains {
         mountains.add(new Mountain("Elbert", 14433));
         mountains.add(new Mountain("Maroon", 14156));
         mountains.add(new Mountain("Castle", 14265));
+        mountains.add(new Mountain("Tallesty", 17894));
         System.out.println("as entered:\n" + mountains);
 
         mountains.sort((mount1, mount2) -> mount1.name.compareTo(mount2.name));
