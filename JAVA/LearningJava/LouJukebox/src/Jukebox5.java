4aa7d4cd42a68e674e424489decaeeb16f0a1b96
sidneirichard <sidneirichardpagliaroni@gmail.com>
2023-07-04 13:23:19 -0300

    Jukebox5 compareTo()

--- JAVA/LearningJava/LouJukebox/src/Jukebox5.java
+++ JAVA/LearningJava/LouJukebox/src/Jukebox5.java
@@ -1,17 +1,18 @@
-/*import java.util.Collections;
 import java.util.*;
-
-public class Jukebox4 {
+public class Jukebox5 {
     public static void main(String[] args) {
-        new Jukebox4().go();
+        new Jukebox5().go();
     }
     public void go() {
         List<SongV3> songList = MockSongs.getSongV3();
         System.out.println(songList);
-        Collections.sort(songList);
+
+        TitleCompare titleCompare = new TitleCompare();
+        songList.sort(titleCompare);
         System.out.println(songList);
+
         ArtistCompare artistCompare = new ArtistCompare();
         songList.sort(artistCompare);
         System.out.println(songList);
     }
-}*/
+}
